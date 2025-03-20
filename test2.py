ce241e9c7eb875113e7c51c85647807337e0a545
9Z-xY7wV6uT <werty4568@naver.com>
2025-03-21 03:28:22 +0900

    work

---
+++ test2.py
@@ -0,0 +1,8 @@
+print("동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리나라 만세")
+print("""무궁화 삼천리 화려강산 대한사람
+대한으로 길이 보전하세""")
+print("이름\t나이\t지역")
+print("\\ \\ \\")
+string=input("입력>")
+output_a = int("52")
+print(type(output_a), output_a)
